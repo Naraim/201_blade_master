91cfbdc629ef3f5d00990e0f6ab28effe4f1f17b
Marian Q <mariansofia1999@hotmail.com>
2020-03-16 15:45:36 -0500

    Updates

---
+++ precache-manifest.88a4db0b890904ea3e827f60b0b251ef.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d00b50d5677a978409e695b2e8df74fb",
+    "url": "/201_blade_master/index.html"
+  },
+  {
+    "revision": "99a1c225d01bc2091536",
+    "url": "/201_blade_master/static/css/main.cea9e08d.chunk.css"
+  },
+  {
+    "revision": "0c1fca6c604767a474df",
+    "url": "/201_blade_master/static/js/2.f9f0db7f.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/201_blade_master/static/js/2.f9f0db7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99a1c225d01bc2091536",
+    "url": "/201_blade_master/static/js/main.a8911e64.chunk.js"
+  },
+  {
+    "revision": "80a59f6be1cd43a6001f",
+    "url": "/201_blade_master/static/js/runtime-main.68d8c4d8.js"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/201_blade_master/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/201_blade_master/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+    "url": "/201_blade_master/static/media/Poppins-SemiBold.342ba3d8.ttf"
+  },
+  {
+    "revision": "317937b0d05f0aaaae6c751d674ade7b",
+    "url": "/201_blade_master/static/media/background-home.317937b0.jpg"
+  },
+  {
+    "revision": "0851cd3916eeef244dd11f59b9cd1a26",
+    "url": "/201_blade_master/static/media/behance-logo.0851cd39.svg"
+  },
+  {
+    "revision": "c4e95598374f31304f7a70ab7ab56dd7",
+    "url": "/201_blade_master/static/media/border-left.c4e95598.svg"
+  },
+  {
+    "revision": "731a3594dbbb2b8565df1bfff5cbd35f",
+    "url": "/201_blade_master/static/media/gallery.731a3594.jpg"
+  },
+  {
+    "revision": "0736056db3a1d09f1df2c2218f4d0183",
+    "url": "/201_blade_master/static/media/github-logo.0736056d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//201_blade_master/precache-manifest.2f670e1c62ea8388cb0d16b4ec96b0dc.js"
+  "/201_blade_master/precache-manifest.88a4db0b890904ea3e827f60b0b251ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//201_blade_master/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/201_blade_master/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
